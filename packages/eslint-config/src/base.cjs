1d556461d20b93359960fdc8650a0277b22e54fc
MH4GF <h.miyagi.cnw@gmail.com>
2023-05-23 22:45:56 +0900

    feat: setup eslint-config

---
+++ packages/eslint-config/src/base.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require('@mh4gf/eslint-config'),
+  parserOptions: { project: './tsconfig.json' },
+}
--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,4 +1,3 @@
-/** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
   root: true,
   extends: ['@project/eslint-config/base'],
