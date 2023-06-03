6210666441792933a2c290b35f2f542ddfd0c977
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-03 16:49:58 +0900

    feat: implement header

--- apps/web/app/_features/Header.tsx
+++ apps/web/app/_features/Header.tsx
@@ -1,27 +1,74 @@
+'use client'
+
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { useState } from 'react'
+
+const navigation = [
+  { name: 'Articles', href: '/articles' },
+  { name: 'About', href: '/' },
+] as const
 
 export const Header = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <header className="flex justify-between items-center">
-      <h1 className="text-4xl font-bold">
-        <Link href="/" className="hover:text-gray-300">
-          mh4gf.dev
-        </Link>
-      </h1>
-      <nav>
-        <ul className="flex space-x-4 mt-4">
-          <li>
-            <Link href="/articles" className="hover:text-gray-300">
-              Articles
-            </Link>
-          </li>
-          <li>
-            <Link href="/" className="hover:text-gray-300">
-              About
-            </Link>
-          </li>
-        </ul>
+    <header>
+      <nav className="mx-auto flex items-center justify-between py-6" aria-label="Global">
+        <div className="flex items-center justify-between w-full gap-x-12">
+          <Link href="/" className="-m-1.5 p-1.5">
+            mh4gf.dev
+          </Link>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <Link key={item.name} href={item.href} className="text-sm font-semibold leading-6">
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+            onClick={() => setOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
       </nav>
+      <Dialog as="div" className="lg:hidden" open={open} onClose={setOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-zinc-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-end">
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   )
 }
