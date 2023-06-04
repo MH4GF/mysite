2bc8612a612b9516939ffa82e3df3a0bf52788ff
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-05 08:23:30 +0900

    refactor: fix to use rootJoin()

--- apps/web/app/articles/[slug]/_features/Article.tsx
+++ apps/web/app/articles/[slug]/_features/Article.tsx
@@ -1,9 +1,7 @@
-import path from 'path'
-
 import { getMarkdownContent } from '@project/markdown-renderer'
 
 import { MarkdownContent } from '../../../_components/MarkdownContent'
-import { env } from '../../../env'
+import { rootJoin } from '../../../_utils/rootJoin'
 
 interface Props {
   slug: string
@@ -12,7 +10,7 @@ interface Props {
 
 // @ts-expect-error Async Server Component
 export const Article = async ({ slug, handleNotFound }: Props): JSX.Element => {
-  const filePath = path.join(env.PNPM_SCRIPT_SRC_DIR, `contents/articles/${slug}.md`)
+  const filePath = rootJoin(`contents/articles/${slug}.md`)
   const article = await getMarkdownContent(filePath, handleNotFound)
 
   return <MarkdownContent>{article}</MarkdownContent>
--- apps/web/app/articles/_features/getArticles.ts
+++ apps/web/app/articles/_features/getArticles.ts
@@ -1,13 +1,13 @@
 import { promises as fs } from 'fs'
-import path from 'path'
 
-import { env } from '../../env'
+import { rootJoin } from '../../_utils/rootJoin'
 
 import type { Article } from './type'
 import { articlesSchema } from './type'
 
 export const getArticles = async (): Promise<Article[]> => {
-  const metadataPath = path.join(env.PNPM_SCRIPT_SRC_DIR, `contents/articles/articles.json`)
+  const metadataPath = rootJoin(`contents/articles/articles.json`)
+
   const file = await fs.readFile(metadataPath, 'utf8')
   return articlesSchema.parse(JSON.parse(file)).articles as Article[]
 }
