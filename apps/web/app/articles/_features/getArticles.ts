ad016d7adf2181c0320548fcc7a6397e5716ceb6
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-04 21:49:00 +0900

    refactor articles page

--- apps/web/app/articles/_features/getArticles.ts
+++ apps/web/app/articles/_features/getArticles.ts
@@ -7,7 +7,7 @@ import type { Article } from './type'
 import { articlesSchema } from './type'
 
 export const getArticles = async (): Promise<Article[]> => {
-  const metadataPath = path.join(env.ARTICLES_PATH, `/articles.json`)
+  const metadataPath = path.join(env.PNPM_SCRIPT_SRC_DIR, `contents/articles/articles.json`)
   const file = await fs.readFile(metadataPath, 'utf8')
   return articlesSchema.parse(JSON.parse(file)).articles as Article[]
 }
