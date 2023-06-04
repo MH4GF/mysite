53e8299af0d13dbd14237dfcd09cceb3087d453b
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-04 20:02:42 +0900

    add ColorModeToggle

--- apps/web/app/_features/Header.tsx
+++ apps/web/app/_features/Header.tsx
@@ -11,16 +11,17 @@ export const Header = () => {
   return (
     <header>
       <nav className="mx-auto flex items-center justify-between py-6" aria-label="Global">
-        <div className="flex items-center justify-between w-full gap-x-12">
+        <div className="flex w-full items-center justify-between gap-x-12">
           <Link href="/" className="-m-1.5 p-1.5">
             mh4gf.dev
           </Link>
-          <div className="hidden lg:flex lg:gap-x-12">
+          <div className="flex items-center gap-x-12">
             {navigation.map((item) => (
               <Link key={item.name} href={item.href} className="text-sm font-semibold leading-6">
                 {item.name}
               </Link>
             ))}
+            <ColorModeToggle />
           </div>
         </div>
       </nav>
