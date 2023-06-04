d65364508f0f189dfc8094e21c74307c60bc67ee
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-04 20:00:58 +0900

    remove mobile menu

--- apps/web/app/_features/Header.tsx
+++ apps/web/app/_features/Header.tsx
@@ -1,9 +1,6 @@
-'use client'
-
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import { useState } from 'react'
+
+import { ColorModeToggle } from './ColorModeToggle'
 
 const navigation = [
   { name: 'Articles', href: '/articles' },
@@ -11,8 +8,6 @@ const navigation = [
 ] as const
 
 export const Header = () => {
-  const [open, setOpen] = useState(false)
-
   return (
     <header>
       <nav className="mx-auto flex items-center justify-between py-6" aria-label="Global">
@@ -28,47 +23,7 @@ export const Header = () => {
             ))}
           </div>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
-            onClick={() => setOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={open} onClose={setOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-zinc-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-end">
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
   )
 }
