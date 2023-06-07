cc74fa63bf731862db69c2d23346f4e04121ba1b
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-07 22:30:43 +0900

    feat: get color theme in first load

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,16 @@
 import '@project/configs/tailwindcss/global.css'
 import { Inter } from 'next/font/google'
 
+import { ColorModeScript } from './_features/ColorModeScript'
 import { Header } from './_features/Header'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${inter.variable} dark:bg-zinc-800`}>
+    <html lang="en" className={`${inter.variable} dark:bg-zinc-800`} suppressHydrationWarning>
       <body className="dark:bg-gradient-to-tr dark:from-zinc-700 dark:via-zinc-900 dark:to-zinc-800 dark:text-zinc-100 ">
+        <ColorModeScript />
         <main className="mx-auto grid min-h-screen max-w-2xl content-start gap-8">
           <Header />
           {children}
