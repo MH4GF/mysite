c3456fe1992bcd9b2d674c3ac460524b9a1d71ce
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-08 08:50:58 +0900

    feat: chore background color

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,12 +8,14 @@ const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-int
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${inter.variable} dark:bg-zinc-800`} suppressHydrationWarning>
-      <body className="dark:bg-gradient-to-tr dark:from-zinc-700 dark:via-zinc-900 dark:to-zinc-800 dark:text-zinc-100 ">
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
+      <body className="dark:bg-zinc-800">
         <ColorModeScript />
-        <main className="mx-auto grid min-h-screen max-w-2xl content-start gap-8">
-          <Header />
-          {children}
+        <main className="dark:bg-gradient-to-tr dark:from-zinc-700 dark:via-zinc-900 dark:to-zinc-800 dark:text-zinc-100">
+          <div className="mx-auto grid min-h-screen max-w-2xl content-start gap-8">
+            <Header />
+            {children}
+          </div>
         </main>
       </body>
     </html>
