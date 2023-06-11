6e0b38f0bcf22ece5625a4c33418576456fd19a0
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-11 15:24:52 +0900

    refactor(env): move to constants

--- apps/web/app/_utils/constants.ts
+++ apps/web/app/_utils/constants.ts
@@ -1,3 +1,16 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    PNPM_SCRIPT_SRC_DIR: z.string(),
+  },
+  client: {},
+  runtimeEnv: {
+    PNPM_SCRIPT_SRC_DIR: process.env['PNPM_SCRIPT_SRC_DIR'],
+  },
+})
+
 export const externalLinks = Object.freeze({
   avatar: 'https://avatars.githubusercontent.com/u/31152321?v=4',
 })
