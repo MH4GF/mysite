f7e12ae38603bf2e921a080f21933cd2265a6c74
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-11 22:02:11 +0900

    feat(RichLinkCard): styling

--- apps/web/app/_features/richLinkCard/RichLinkCard.tsx
+++ apps/web/app/_features/richLinkCard/RichLinkCard.tsx
@@ -1,23 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
 import { useId } from 'react'
 
-interface RichLinkCardInnerProps {
-  url: string
-  title?: string
-  description?: string
-  imageSrc?: string
-}
+import { getOGP } from './getOGP'
+import type { OGPResult } from './types'
+
+type RichLinkCardInnerProps = OGPResult
 
 const RichLinkCardInner = ({ url, title, description, imageSrc }: RichLinkCardInnerProps) => {
   const labelledBy = useId()
 
   return (
-    <section aria-labelledby={labelledBy}>
-      <a href={url} target="_blank" rel="noreferrer">
-        <div>
-          <h1 id={labelledBy}>{title}</h1>
-          <p>{description}</p>
+    <section
+      className={`not-prose h-24 w-full rounded-sm border border-solid hover:cursor-pointer`}
+      aria-labelledby={labelledBy}
+    >
+      <a
+        className="grid h-full grid-flow-row-dense grid-cols-4"
+        href={url}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="col-span-3 flex flex-col gap-2 overflow-hidden p-2">
+          <h1 className="overflow-hidden text-ellipsis text-base font-bold" id={labelledBy}>
+            {title}
+          </h1>
+          <p className="overflow-hidden text-sm">{description}</p>
         </div>
-        <img src={imageSrc} alt={title} />
+        <img className="col-span-1 h-full w-full object-cover" src={imageSrc} alt={title} />
       </a>
     </section>
   )
@@ -27,6 +36,7 @@ interface Props {
   url: string
 }
 
-export const RichLinkCard = ({ url }: Props) => {
-  return <RichLinkCardInner url={url} />
+export const RichLinkCard = async ({ url }: Props) => {
+  const result = await getOGP(url)
+  return <RichLinkCardInner {...result} />
 }
