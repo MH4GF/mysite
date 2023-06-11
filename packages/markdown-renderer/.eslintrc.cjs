7db5ee4a58134e1c8ba01a543567ae99ffe3f39f
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-11 20:42:18 +0900

    refactor: remove markdown-renderer package

--- packages/markdown-renderer/.eslintrc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@project/eslint-config/base'],
-}
--- packages/markdown-renderer/src/processor.ts
+++
@@ -1,9 +0,0 @@
-import { createElement } from 'react'
-import rehypeReact from 'rehype-react'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import { unified } from 'unified'
-
-export const processor = unified().use(remarkParse).use(remarkRehype).use(rehypeReact, {
-  createElement,
-})
