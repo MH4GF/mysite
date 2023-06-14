aac29a79fa67300fa56c6130ecd407f26848f755
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-14 09:14:03 +0900

    feat: add playwright

---
+++ apps/web/playwright.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: 'e2e',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env['CI'],
+  retries: 0,
+  workers: process.env['CI'] ? 1 : '100%',
+  reporter: process.env['CI'] ? [['github'], ['list']] : 'list',
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    port: 3000,
+    reuseExistingServer: !process.env['CI'],
+  },
+})
