63a5c0f8ab6b2467dd329a080c301c9c8daf481e
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-14 09:52:58 +0900

    fix: change the way we get the root path

--- apps/web/app/_utils/constants.ts
+++ apps/web/app/_utils/constants.ts
@@ -3,11 +3,11 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    PNPM_SCRIPT_SRC_DIR: z.string(),
+    PWD: z.string(),
   },
   client: {},
   runtimeEnv: {
-    PNPM_SCRIPT_SRC_DIR: process.env['PNPM_SCRIPT_SRC_DIR'],
+    PWD: process.env['PWD'],
   },
 })
 
--- apps/web/app/_utils/rootJoin.ts
+++ apps/web/app/_utils/rootJoin.ts
@@ -2,4 +2,4 @@ import path from 'path'
 
 import { env } from './constants'
 
-export const rootJoin = (...args: string[]) => path.join(env.PNPM_SCRIPT_SRC_DIR, ...args)
+export const rootJoin = (...args: string[]) => path.join(env.PWD, ...args)
