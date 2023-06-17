4425bf05df5e45d8d2050efce1611510f6e9f66b
MH4GF <h.miyagi.cnw@gmail.com>
2023-06-17 15:17:19 +0900

    fix: remove firefox and safari

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -20,23 +20,9 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
     },
   ],
   webServer: {
