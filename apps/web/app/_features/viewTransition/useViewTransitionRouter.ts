c594803c67873fbb1992d19e16e9365b2387b0e5
MH4GF <h.miyagi.cnw@gmail.com>
2023-12-28 09:23:41 +0900

    refactor: fix useViewTransitionRouter to include safeStartViewTransition

--- apps/web/app/_features/viewTransition/useViewTransitionRouter.ts
+++ apps/web/app/_features/viewTransition/useViewTransitionRouter.ts
@@ -4,14 +4,21 @@ import type { Route } from "next";
 import type { NavigateOptions } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useRouter as useNextRouter } from "next/navigation";
 
+const safeStartViewTransition = (callback: () => void) => {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (!document.startViewTransition) {
+    callback();
+    return;
+  }
+  document.startViewTransition(callback);
+};
+
 export const useViewTransitionRouter = (): ReturnType<typeof useNextRouter> => {
   const router = useNextRouter();
   return {
     ...router,
     push: (href: Route, options?: NavigateOptions) => {
-      document.startViewTransition(() => {
-        router.push(href, options);
-      });
+      safeStartViewTransition(() => router.push(href, options));
     },
   };
 };
