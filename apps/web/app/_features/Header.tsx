6fa99f2b137c2d3665ba5ecfd48c9f60b0938ed0
MH4GF <h.miyagi.cnw@gmail.com>
2023-12-31 14:11:37 +0900

    fix: Headerにリポジトリへのリンクを追加

--- apps/web/app/_features/Header.tsx
+++ apps/web/app/_features/Header.tsx
@@ -1,5 +1,7 @@
 import { Box, HStack, Text } from "@kuma-ui/core";
 
+import { GitHubIcon, Icon } from "../_components/Icons";
+
 import { ColorModeToggle } from "./colorMode";
 import { Link } from "./viewTransition";
 
@@ -20,12 +22,21 @@ export const Header = () => {
           <Box as={Link} href="/" p={"0.375rem"} m={"-0.375rem"}>
             mh4gf.dev
           </Box>
-          <HStack alignItems={"center"} gap={["1.5rem", "3rem"]}>
+          <HStack alignItems={"center"} gap={"1.5rem"}>
             {navigation.map((item) => (
               <Text variant="sm" as={Link} key={item.name} href={item.href}>
                 {item.name}
               </Text>
             ))}
+            <Box
+              as="a"
+              href="https://github.com/MH4GF/mysite"
+              target="_blank"
+              rel="noreferrer"
+              className="group"
+            >
+              <Icon as={GitHubIcon} />
+            </Box>
             <ColorModeToggle />
           </HStack>
         </HStack>
