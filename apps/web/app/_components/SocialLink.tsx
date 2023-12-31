d470f303d8704e365e09bbd36f17c548d653f4fa
MH4GF <h.miyagi.cnw@gmail.com>
2023-12-31 14:09:14 +0900

    fix: Iconコンポーネントを追加

--- apps/web/app/_components/SocialLink.tsx
+++ apps/web/app/_components/SocialLink.tsx
@@ -1,16 +1,16 @@
 import type { IconComponent } from "./Icons";
-import { GitHubIcon, TwitterIcon } from "./Icons";
+import { GitHubIcon, Icon, TwitterIcon } from "./Icons";
 
 type SocialKind = "twitter" | "github";
-type Socials = Record<SocialKind, { Icon: IconComponent; url: string }>;
+type Socials = Record<SocialKind, { icon: IconComponent; url: string }>;
 
 const socials: Socials = {
   twitter: {
-    Icon: TwitterIcon,
+    icon: TwitterIcon,
     url: "https://twitter.com/MH4GF",
   },
   github: {
-    Icon: GitHubIcon,
+    icon: GitHubIcon,
     url: "https://github.com/MH4GF",
   },
 };
@@ -20,11 +20,11 @@ interface Props {
 }
 
 export const SocialLink = ({ kind }: Props) => {
-  const { Icon, url } = socials[kind];
+  const { icon, url } = socials[kind];
 
   return (
     <a className="group -m-1 p-1" href={url}>
-      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+      <Icon as={icon} />
     </a>
   );
 };
