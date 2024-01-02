c40f1ee637ee1dee696ee2c54b2f57df8a613530
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-02 10:17:02 +0900

    refactor: fix format

--- apps/web/app/_features/articles/getArticleMeta.ts
+++ apps/web/app/_features/articles/getArticleMeta.ts
@@ -1,9 +1,7 @@
 import { getArticlesMeta } from "./getArticlesMeta";
 import type { ArticleMeta } from "./type";
 
-export const getArticleMeta = async (
-  slug: string
-): Promise<ArticleMeta | undefined> => {
+export const getArticleMeta = async (slug: string): Promise<ArticleMeta | undefined> => {
   const articlesMeta = await getArticlesMeta();
   const href = `/articles/${slug}`;
   return articlesMeta.find((articleMeta) => articleMeta.href === href);
