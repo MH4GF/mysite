1786a605d81d1c3c5582daac195f86d7d4e21ccb
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-02 10:08:48 +0900

    feat: show meta for article

--- apps/web/app/articles/[slug]/_features/Article.tsx
+++ apps/web/app/articles/[slug]/_features/Article.tsx
@@ -1,13 +1,41 @@
-import { MarkdownContent } from "@/app/_features";
-import { rootJoin } from "@/app/_utils";
+import { Box, Heading } from "@kuma-ui/core";
+
+import type { ArticleMeta } from "@/app/_features";
+import { MarkdownContent, getArticleMeta } from "@/app/_features";
+import { format, rootJoin } from "@/app/_utils";
+
+const ArticleMetaDetail = ({
+  meta: { publishedAt: _publishedAt, title },
+}: { meta: ArticleMeta }) => {
+  const publishedAt = format(_publishedAt);
+
+  return (
+    <div>
+      <Heading variant="xl4" fontWeight={800}>
+        {title}
+      </Heading>
+      <Box display="flex" justifyContent="flex-end">
+        <Box as="time" dateTime={publishedAt} color={"colors.zinc.500"}>
+          {publishedAt}
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
 interface Props {
   slug: string;
   handleNotFound: () => void;
 }
 
-export const Article = ({ slug, handleNotFound }: Props): JSX.Element => {
+export const Article = async ({ slug, handleNotFound }: Props) => {
   const filePath = rootJoin(`contents/articles/${slug}.md`);
+  const meta = await getArticleMeta(slug);
 
-  return <MarkdownContent filePath={filePath} handleNotFound={handleNotFound} />;
+  return (
+    <>
+      {meta && <ArticleMetaDetail meta={meta} />}
+      <MarkdownContent filePath={filePath} handleNotFound={handleNotFound} />
+    </>
+  );
 };
