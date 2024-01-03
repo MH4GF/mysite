64b32bd72b1e197d0789a3950600db9b07afb0e5
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-03 11:15:00 +0900

    feat: setup font for storybook

--- apps/web/.storybook/preview.ts
+++ apps/web/.storybook/preview.ts
@@ -1,8 +1,10 @@
 import "@project/configs/tailwindcss/global.css";
 
 import type { Preview } from "@storybook/react";
+import { decorators } from "./decorators";
 
 const preview: Preview = {
+  decorators,
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
