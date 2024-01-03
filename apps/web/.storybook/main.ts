625db5d6109bd50f3f007f678d9484584e3b7228
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-03 10:32:24 +0900

    fix: change entrypoint for story files

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -10,7 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../**/*.stories.tsx"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
