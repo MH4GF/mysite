d07920730bb627d1f9773dbd4b5473ee7ed0e3ba
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-05 13:57:14 +0900

    refactor(ColorModeToggle): remove kuma-ui

--- apps/web/app/_features/colorMode/ColorModeToggle.tsx
+++ apps/web/app/_features/colorMode/ColorModeToggle.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Button } from "@kuma-ui/core";
-
 import { MoonIcon, SunIcon } from "@/app/_components";
 
 export const ColorModeToggle = () => {
@@ -28,21 +26,14 @@ export const ColorModeToggle = () => {
   }
 
   return (
-    <Button
+    <button
       type="button"
       aria-label="Toggle dark mode"
-      paddingY="0.5rem"
-      paddingX="0.75rem"
-      borderRadius="0.375rem"
-      backgroundColor="rgb(255 255 255 / 0.9)"
-      backdropFilter="blur(8px)"
-      transitionDuration="150ms"
-      transitionTimingFunction="cubic-bezier(0.4, 0, 0.2, 1)"
-      className="group ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+      className="group rounded-md bg-white/90 px-3 py-2 ring-1 ring-zinc-900/5 backdrop-blur-[8px] transition duration-150 ease-in-out dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
       onClick={toggleMode}
     >
       <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
       <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
-    </Button>
+    </button>
   );
 };
