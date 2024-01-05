d6e11a5fda9fe7ed0741b8d7a56253e1b4d92746
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-05 13:48:18 +0900

    refactor(Footer): remove kuma-ui

--- apps/web/app/_components/Footer.tsx
+++ apps/web/app/_components/Footer.tsx
@@ -1,22 +1,16 @@
-import { Box, Heading, Link as KumaLink, Spacer, Text } from "@kuma-ui/core";
-
 import { MyAvatar } from "@/app/_components";
 import { Link } from "@/app/_features/viewTransition";
 
 export const Footer = () => {
   return (
     <footer>
-      <Box as="section" textAlign={"center"}>
-        <KumaLink as={Link} href="/">
+      <section className="mb-4 text-center">
+        <Link href="/">
           <MyAvatar />
-          <Spacer size={"0.5rem"} />
-          <Heading fontWeight={"bold"}>Hirotaka Miyagi</Heading>
-          <Text variant="sm" color="colors.slate.400">
-            Software Engineer / @mh4gf
-          </Text>
-        </KumaLink>
-      </Box>
-      <Spacer size={"1rem"} />
+          <h2 className="mt-2 font-bold">Hirotaka Miyagi</h2>
+          <p className="text-sm text-slate-400">Software Engineer / @mh4gf</p>
+        </Link>
+      </section>
     </footer>
   );
 };
