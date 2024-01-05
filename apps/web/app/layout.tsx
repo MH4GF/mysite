f26c37234deddcfa8c451f255f161e14394f124c
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-05 13:45:30 +0900

    refactor(layout): change to tailwindcss

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Box, Spacer } from "@kuma-ui/core";
 import { KumaRegistry } from "@kuma-ui/next-plugin/registry";
 import "@project/configs/tailwindcss/global.css";
 import type { Metadata } from "next";
@@ -39,19 +38,18 @@ const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-int
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja" className={inter.variable} suppressHydrationWarning>
-      <Box as="body" className="dark:bg-zinc-800">
+      <body className="dark:bg-zinc-800">
         <KumaRegistry>
           <ColorModeScript />
           <TwitterWidgets />
           <main className="dark:bg-gradient-to-tr dark:from-zinc-700 dark:via-zinc-900 dark:to-zinc-800 dark:text-zinc-100">
-            <Box px={["1rem", 0]} mx={"auto"} minHeight={"100vh"} maxWidth={["", "42rem"]}>
+            <div className="mx-auto flex min-h-screen max-w-2xl flex-col gap-8 px-4 sm:px-0">
               <Header />
-              <Spacer size={"2rem"} />
-              {children}
-            </Box>
+              <div>{children}</div>
+            </div>
           </main>
         </KumaRegistry>
-      </Box>
+      </body>
     </html>
   );
 }
