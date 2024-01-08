78da60e90ab4bca77595de204c8df0d1d8737a34
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-08 21:54:20 +0900

    refactor(Tag): remove unneccesary dependencies

--- apps/web/app/articles/_features/Tag.tsx
+++ apps/web/app/articles/_features/Tag.tsx
@@ -1,23 +1,31 @@
-import clsx from "clsx";
-import { match } from "ts-pattern";
-
 import type { TagEnum } from "@/app/_features";
+
+const getColorForTag = (tag: TagEnum): string => {
+  switch (tag) {
+    case "ROUTE06 Tech Blog":
+      return "bg-blue-600 dark:bg-blue-900";
+    case "Timee Product Team Blog":
+      return "bg-yellow-400 dark:bg-yellow-600";
+    case "Zenn":
+      return "bg-sky-600 dark:bg-sky-700";
+    case "Dev":
+      return "bg-orange-600 dark:bg-orange-700";
+    case "Life":
+      return "bg-rose-700";
+    case "note":
+      return "bg-green-600 dark:bg-green-700";
+    case "Qiita":
+    case "Speaker Deck":
+      return "bg-teal-500 dark:bg-teal-700";
+  }
+};
 
 interface Props {
   tag: TagEnum;
 }
 
 export const Tag = ({ tag }: Props) => {
-  const colors = match(tag)
-    .with("ROUTE06 Tech Blog", () => ["bg-blue-600", "dark:bg-blue-900"])
-    .with("Timee Product Team Blog", () => ["bg-yellow-400", "dark:bg-yellow-600"])
-    .with("Zenn", () => ["bg-sky-600", "dark:bg-sky-700"])
-    .with("Dev", () => ["bg-orange-600", "dark:bg-orange-700"])
-    .with("Life", () => ["bg-rose-700"])
-    .with("note", () => ["bg-green-600", "dark:bg-green-700"])
-    .with("Qiita", () => ["bg-teal-500", "dark:bg-teal-700"])
-    .with("Speaker Deck", () => ["bg-teal-500", "dark:bg-teal-700"])
-    .exhaustive();
+  const colors = getColorForTag(tag);
 
-  return <span className={clsx(colors, "rounded px-1 py-px text-sm text-white")}>{tag}</span>;
+  return <span className={`${colors} rounded px-1 py-px text-sm text-white`}>{tag}</span>;
 };
