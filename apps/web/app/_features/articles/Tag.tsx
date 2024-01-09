a42ce69084017882e4db19541421d8f76aaed0ea
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-09 09:15:30 +0900

    refactor: refactor to tagColorMap

--- apps/web/app/_features/articles/Tag.tsx
+++ apps/web/app/_features/articles/Tag.tsx
@@ -1,31 +1,22 @@
 import type { TagEnum } from "@/app/_features";
 
-const getColorForTag = (tag: TagEnum): string => {
-  switch (tag) {
-    case "ROUTE06 Tech Blog":
-      return "bg-blue-600 dark:bg-blue-900";
-    case "Timee Product Team Blog":
-      return "bg-yellow-400 dark:bg-yellow-600";
-    case "Zenn":
-      return "bg-sky-600 dark:bg-sky-700";
-    case "Dev":
-      return "bg-orange-600 dark:bg-orange-700";
-    case "Life":
-      return "bg-rose-700";
-    case "note":
-      return "bg-green-600 dark:bg-green-700";
-    case "Qiita":
-    case "Speaker Deck":
-      return "bg-teal-500 dark:bg-teal-700";
-  }
-};
+const tagColorMap: Record<TagEnum, string> = {
+  "ROUTE06 Tech Blog": "bg-blue-600 dark:bg-blue-900",
+  "Timee Product Team Blog": "bg-yellow-400 dark:bg-yellow-600",
+  Zenn: "bg-sky-600 dark:bg-sky-700",
+  Dev: "bg-orange-600 dark:bg-orange-700",
+  Life: "bg-rose-700",
+  note: "bg-green-600 dark:bg-green-700",
+  Qiita: "bg-teal-500 dark:bg-teal-700",
+  "Speaker Deck": "bg-teal-500 dark:bg-teal-700",
+} as const;
 
 interface Props {
   tag: TagEnum;
 }
 
 export const Tag = ({ tag }: Props) => {
-  const colors = getColorForTag(tag);
+  const colors = tagColorMap[tag];
 
   return <span className={`${colors} rounded px-1 py-px text-sm text-white`}>{tag}</span>;
 };
