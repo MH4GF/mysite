19af719ab748e466a9fb5ae8a27f9ae834be59ea
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-11 22:36:55 +0900

    fix: fix Link component for same page

--- apps/web/app/_features/viewTransition/Link.tsx
+++ apps/web/app/_features/viewTransition/Link.tsx
@@ -8,8 +8,19 @@ import NextLink from "next/link";
 
 import { useViewTransitionRouter } from "./useViewTransitionRouter";
 
+const isSamePage = (href: string) => href.startsWith("#");
+
 export function Link<T extends string = string>({ children, href, ...props }: LinkProps<T>) {
   const router = useViewTransitionRouter();
+
+  // 同一ページ内での遷移の場合はトランジションを追加すると不安定なため、単純にNextLinkで遷移する
+  if (isSamePage(href.toString())) {
+    return (
+      <NextLink {...props} href={href}>
+        {children}
+      </NextLink>
+    );
+  }
 
   const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
