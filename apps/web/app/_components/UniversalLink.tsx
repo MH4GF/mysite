a11350208a89f164cc1c7f2f2ba96848ed401cba
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-17 08:38:28 +0900

    feat: animated underline on links

--- apps/web/app/_components/UniversalLink.tsx
+++ apps/web/app/_components/UniversalLink.tsx
@@ -30,14 +30,25 @@ const isSameOrigin = <T extends string = string>(_href: Route<T> | UrlObject) =>
   return url.origin === baseOrigin;
 };
 
+const animatedUnderline =
+  "no-underline transition-colors duration-300 border-b border-solid border-zinc-200 hover:border-zinc-800 dark:border-zinc-700 dark:hover:border-zinc-300";
+
 type Props<T extends string = string> = LinkProps<T> & {
   isExternal?: boolean;
+  isEnabledUnderline?: boolean;
 };
 
-export function UniversalLink<T extends string = string>({ isExternal, ...props }: Props<T>) {
+export function UniversalLink<T extends string = string>({
+  isExternal,
+  isEnabledUnderline = false,
+  className: _className,
+  ...props
+}: Props<T>) {
+  const className = `${_className || ""} ${isEnabledUnderline ? animatedUnderline : ""}`;
+
   if (isExternal || !isSameOrigin<T>(props.href)) {
-    return <NextLink target="_blank" rel="noreferrer" {...props} />;
+    return <NextLink target="_blank" rel="noreferrer" className={className} {...props} />;
   }
 
-  return <Link {...props} />;
+  return <Link className={className} {...props} />;
 }
