246465751be8a49462015f8f2d566a7b474db30f
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-21 19:10:10 +0900

    fix: change to dynamic import for share feature

--- apps/web/app/articles/[slug]/_features/ArticleShareButton.tsx
+++ apps/web/app/articles/[slug]/_features/ArticleShareButton.tsx
@@ -1,6 +1,9 @@
+import dynamic from "next/dynamic";
+
 import type { ArticleMeta } from "@/app/_features";
-import { Share } from "@/app/_features";
 import { baseUrl } from "@/app/_utils";
+
+const Share = dynamic(() => import("@/app/_features/share/Share"), { ssr: false });
 
 interface Props {
   meta: ArticleMeta;
