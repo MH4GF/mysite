d9673277ba3a0950a35a3bdb490dc53f7de2c1c4
MH4GF <h.miyagi.cnw@gmail.com>
2024-01-21 17:02:54 +0900

    fix base url for production

--- apps/web/app/_utils/constants.ts
+++ apps/web/app/_utils/constants.ts
@@ -9,7 +9,8 @@ export const siteInfo = Object.freeze({
   twitter: "@mh4gf",
 });
 
-export const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  ? process.env.NEXT_PUBLIC_BASE_URL
+  : process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000";
