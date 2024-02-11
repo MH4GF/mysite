56ebec78918dd5730de8a674df9cf3cd151cb313
MH4GF <h.miyagi.cnw@gmail.com>
2024-02-11 13:48:37 +0900

    fix: lastmodを記事ページにのみ設定し、その他のページでは設定しない
    ref: https://zenn.dev/link/comments/508055225275cc

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -3,37 +3,30 @@ import type { MetadataRoute } from "next";
 import { tagList } from "./_features";
 import { baseUrl } from "./_utils";
 
-import { allDocuments } from "contentlayer/generated";
+import { allAbouts, allArticles } from "contentlayer/generated";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticPaths: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
     },
     {
       url: `${baseUrl}/articles`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
   ];
 
-  const documentPaths: MetadataRoute.Sitemap = allDocuments.map((document) => ({
-    url: `${baseUrl}${document.href}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.8,
+  const articlePaths: MetadataRoute.Sitemap = allArticles.map((article) => ({
+    url: `${baseUrl}${article.href}`,
+    lastModified: new Date(article.publishedAt),
+  }));
+
+  const aboutPaths: MetadataRoute.Sitemap = allAbouts.map((article) => ({
+    url: `${baseUrl}${article.href}`,
   }));
 
   const tagPaths: MetadataRoute.Sitemap = tagList.map((tag) => ({
     url: `${baseUrl}/articles/tags/${tag}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.8,
   }));
 
-  return [...staticPaths, ...documentPaths, ...tagPaths];
+  return [...staticPaths, ...articlePaths, ...aboutPaths, ...tagPaths];
 }
