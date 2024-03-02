792a5ce955282d6fbee52893f42e49d9e60b45dc
MH4GF <h.miyagi.cnw@gmail.com>
2024-03-02 17:47:45 +0900

    fix: remove eslint-plugin-import-access

--- apps/web/app/_components/UniversalLink.tsx
+++ apps/web/app/_components/UniversalLink.tsx
@@ -2,7 +2,6 @@ import type { UrlObject } from "url";
 
 import type { Route } from "next";
 import type { LinkProps } from "next/link";
-// eslint-disable-next-line import-access/jsdoc
 import NextLink from "next/link";
 
 import { Link } from "../_features/viewTransition";
--- apps/web/app/_features/viewTransition/useViewTransitionRouter.ts
+++ apps/web/app/_features/viewTransition/useViewTransitionRouter.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import type { Route } from "next";
-// eslint-disable-next-line import-access/jsdoc
 import { usePathname, useRouter } from "next/navigation";
 import { useLayoutEffect, useRef } from "react";
 
