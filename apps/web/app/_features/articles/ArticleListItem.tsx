f6351cdd976f5bd2fa9e8778501ce7a37dfd249f
MH4GF <h.miyagi.cnw@gmail.com>
2024-03-12 10:22:04 +0900

    fix: xmlnsは効果がないため削除(ineffective-attr)

--- apps/web/app/_features/articles/ArticleListItem.tsx
+++ apps/web/app/_features/articles/ArticleListItem.tsx
@@ -27,7 +27,6 @@ export const ArticleListItem = <T extends string>({
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
             >
               <title>External Link</title>
               <path
