8bc06c7fc15eddfe545d4cc773035a8ced373715
MH4GF <h.miyagi.cnw@gmail.com>
2024-03-16 15:20:16 +0900

    chore: 不要なdivを取り除く

--- apps/web/app/_features/Header.tsx
+++ apps/web/app/_features/Header.tsx
@@ -8,26 +8,27 @@ const navigation = [{ name: "Articles", href: "/articles" }] as const;
 export const Header = () => {
   return (
     <header>
-      <nav className="mx-auto flex items-center justify-between py-6" aria-label="ヘッダー">
-        <div className="flex w-full items-center justify-between gap-12">
-          <UniversalLink href="/" className="-m-1.5 p-1.5">
-            mh4gf.dev
-          </UniversalLink>
-          <div className="flex items-center gap-6">
-            {navigation.map((item) => (
-              <UniversalLink href={item.href} key={item.name}>
-                {item.name}
-              </UniversalLink>
-            ))}
-            <UniversalLink
-              href="https://github.com/MH4GF/mysite"
-              className="group"
-              aria-label="このサイトのGitHubリポジトリ"
-            >
-              <GitHubIcon className="h-6 w-6" />
+      <nav
+        className="mx-auto flex w-full items-center justify-between gap-12 py-6"
+        aria-label="ヘッダー"
+      >
+        <UniversalLink href="/" className="-m-1.5 p-1.5">
+          mh4gf.dev
+        </UniversalLink>
+        <div className="flex items-center gap-6">
+          {navigation.map((item) => (
+            <UniversalLink href={item.href} key={item.name}>
+              {item.name}
             </UniversalLink>
-            <ColorModeToggle />
-          </div>
+          ))}
+          <UniversalLink
+            href="https://github.com/MH4GF/mysite"
+            className="group"
+            aria-label="このサイトのGitHubリポジトリ"
+          >
+            <GitHubIcon className="h-6 w-6" />
+          </UniversalLink>
+          <ColorModeToggle />
         </div>
       </nav>
     </header>
