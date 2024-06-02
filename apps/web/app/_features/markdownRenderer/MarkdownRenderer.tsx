211100fe17c3d96f6b3677c51ad02e2eb25bef15
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-02 12:50:12 +0900

    chore: update unified packages

--- apps/web/app/_features/markdownRenderer/MarkdownRenderer.tsx
+++ apps/web/app/_features/markdownRenderer/MarkdownRenderer.tsx
@@ -7,7 +7,6 @@ interface Props {
 }
 
 export const MarkdownRenderer = async ({ raw }: Props): Promise<JSX.Element> => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   const { result } = await processor.process(raw);
 
   return <article className="prose prose-zinc dark:prose-invert">{result}</article>;
