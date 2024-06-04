b6d655fd80707fa76e38d5179e6c6cd20d06a18e
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-04 23:28:26 +0900

    fix: migrate eslint-config

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,5 +1,20 @@
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
   root: true,
-  extends: ["@project/eslint-config/next"],
+  extends: ["@mh4gf/eslint-config", "plugin:@next/next/recommended"],
+  parserOptions: { project: "./tsconfig.json" },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  rules: {
+    // tsのnoPropertyAccessFromIndexSignatureと競合するためオフにする
+    // @see: https://typescript-eslint.io/rules/dot-notation/
+    // @see: https://typescriptbook.jp/reference/tsconfig/nopropertyaccessfromindexsignature
+    "@typescript-eslint/dot-notation": "off",
+
+    // biomeへ移行するためオフにする
+    "@typescript-eslint/no-empty-interface": "off",
+  },
 };
