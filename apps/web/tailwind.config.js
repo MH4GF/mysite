12850c10d0bbd017bcb6f590b1aa5f653417d463
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-10 10:01:29 +0900

    fix color

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -54,8 +54,9 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "foreground-sub": "var(--foreground-sub)",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
