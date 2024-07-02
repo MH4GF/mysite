ff654355d2498df2a279a070056dd7501dcced23
MH4GF <h.miyagi.cnw@gmail.com>
2024-07-02 09:35:52 +0900

    fix ci

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,7 @@ import type { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   customShots: {
-    currentShotsPath: "apps/web/app/__vrt__/screenshots",
+    currentShotsPath: "app/__vrt__/screenshots",
   },
   lostPixelProjectId: "clrle4r1n14nfnj0e2ij7un7c",
   apiKey: process.env.LOST_PIXEL_API_KEY,
