041693781187c504afb5531e3333870c9c4c675e
MH4GF <h.miyagi.cnw@gmail.com>
2024-07-07 18:26:08 +0900

    maintenance: ignore eslint and tsc in build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     typedRoutes: true,
   },
