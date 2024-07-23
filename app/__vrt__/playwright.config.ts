0ca625fec362c7037378001bcd71a9ae55e64266
MH4GF <h.miyagi.cnw@gmail.com>
2024-07-23 09:31:11 +0900

    maintenance: rename vrt test file

--- app/__vrt__/playwright.config.ts
+++ app/__vrt__/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testMatch: "vrt.test.ts",
+  testMatch: "test.vrt.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
