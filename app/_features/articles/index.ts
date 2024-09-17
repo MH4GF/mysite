29efc5857f2fec6e635b30bfbef598858536eb5b
MH4GF <h.miyagi.cnw@gmail.com>
2024-09-17 09:17:09 +0900

    chore(Article): Remove TableOfContents component and update dependencies

--- app/_features/articles/index.ts
+++ app/_features/articles/index.ts
@@ -3,6 +3,5 @@ export { Tag } from "./Tag";
 export type { ArticleMeta } from "./type";
 export { tagList, tagsSchema } from "./type";
 export { tagLabelMap } from "./constants";
-export { TableOfContents } from "./toc";
 export { getArticlesMeta } from "./getArticlesMeta";
 export { ArticleList } from "./ArticleList";
--- app/_features/articles/toc/index.ts
+++ app/_features/articles/toc/index.ts
@@ -1,2 +1,1 @@
-export { TableOfContents } from "./TableOfContents";
 export { MarkdownToToc } from "./MarkdownToToc";
