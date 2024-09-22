e45ef129db0893ed81a8d6859ea6930b41090559
MH4GF <h.miyagi.cnw@gmail.com>
2024-09-22 21:08:35 +0900

    feat(ui): add dialog component to MyAvatar

--- app/_components/MyAvatar.tsx
+++ app/_components/MyAvatar.tsx
@@ -1,17 +1,40 @@
 import Image from "next/image";
 
 import avatar from "@/public/images/avatar.jpeg";
+import { Root as VisuallyHiddenRoot } from "@radix-ui/react-visually-hidden";
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 export const MyAvatar = () => {
   return (
-    <Image
-      src={avatar}
-      alt="MH4GFのプロフィール画像"
-      width="32"
-      height="32"
-      placeholder="blur"
-      className="inline-block rounded-full"
-      priority
-    />
+    <Dialog>
+      <DialogTrigger>
+        <Image
+          src={avatar}
+          alt="MH4GF profile picture"
+          width="32"
+          height="32"
+          placeholder="blur"
+          className="inline-block rounded-full"
+          priority
+        />
+      </DialogTrigger>
+      <DialogContent className="border-none bg-transparent justify-items-center">
+        <Image
+          src={avatar}
+          alt="MH4GF profile picture"
+          width="256"
+          height="256"
+          placeholder="blur"
+          className="rounded-full"
+          priority
+        />
+        <VisuallyHiddenRoot>
+          <DialogTitle>MH4GF profile picture</DialogTitle>
+        </VisuallyHiddenRoot>
+        <DialogDescription>
+          MH4GF profile picture. The rabbit in the front, his name is Ponpoko.
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   );
 };
