e1803f93d5e73a4b8a71baa9aea2bb030e9dc147
MH4GF <h.miyagi.cnw@gmail.com>
2024-12-22 14:45:24 +0900

    fix: remove UniversalLink from Tag component and use span instead

--- app/_features/articles/Tag.tsx
+++ app/_features/articles/Tag.tsx
@@ -1,5 +1,3 @@
-import { UniversalLink } from "../viewTransition";
-
 import { tagLabelMap } from "./constants";
 import type { TagEnum } from "./type";
 
@@ -11,12 +9,8 @@ export const Tag = ({ tag }: Props) => {
   const label = tagLabelMap[tag];
 
   return (
-    <UniversalLink
-      href={`/articles/tags/${tag}`}
-      isEnabledUnderline
-      className="rounded-sm border border-zinc-200 px-1 py-0.5 text-xs text-zinc-500 dark:hover:text-zinc-500 dark:text-zinc-400 hover:text-zinc-700"
-    >
+    <span className="rounded-sm border border-zinc-600 px-1 py-0.5 text-xs text-zinc-500">
       {label}
-    </UniversalLink>
+    </span>
   );
 };
