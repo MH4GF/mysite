21ff6101d0a262d9b25b1b1abaaa52f8cdd348bc
MH4GF <h.miyagi.cnw@gmail.com>
2025-01-01 20:26:08 +0900

    chore: remove unused exports

--- app/_components/index.ts
+++ app/_components/index.ts
@@ -1,4 +1,3 @@
-export { RssIcon } from "./icons";
 export { MyAvatar } from "./MyAvatar";
 export { Footer } from "./Footer";
 export { Time } from "./Time";
--- app/_features/articles/index.ts
+++ app/_features/articles/index.ts
@@ -1,5 +1,4 @@
 export { getArticle } from "./getArticle";
-export { Tag } from "./Tag";
 export type { ArticleMeta } from "./type";
 export { tagList, tagsSchema } from "./type";
 export { tagLabelMap } from "./constants";
