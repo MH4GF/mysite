d1d2f6b1bd51e165e516fefaff11411235be3522
MH4GF <h.miyagi.cnw@gmail.com>
2025-12-08 20:51:56 +0900

    debug: Add large red banner to verify VRT detects differences

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,8 +1,13 @@
 export const Footer = ({ className }: { className?: string }) => {
   return (
-    <footer className={`${className} flex border-zinc-200 border-t p-4 dark:border-zinc-600`}>
+    <footer
+      className={`${className} flex flex-col border-zinc-200 border-t p-4 dark:border-zinc-600`}
+    >
+      <div className="bg-red-500 text-white text-2xl font-bold p-4 mb-2">
+        DEBUG: VRT SHOULD FAIL
+      </div>
       <p className="text-xs text-zinc-500 leading-5 dark:text-zinc-400">
-        &copy; 2024 Hirotaka Miyagi, All rights reserved. [DEBUG: VRT TEST]
+        &copy; 2024 Hirotaka Miyagi, All rights reserved.
       </p>
     </footer>
   );
